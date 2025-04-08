5b9751b3758c326bebd82e788af55f3fbbd6d794
Ivan Lapin <104844376+Ivan-Lapin@users.noreply.github.com>
2025-04-08 17:25:04 +0300

    Add map as storage, output date, change worker(1 in 24 hour)

--- currency/main.go
+++ currency/main.go
@@ -8,16 +8,24 @@ import (
 	"time"
 )
 
+var ADRESS_API = "https://v6.exchangerate-api.com/v6/1d40c79e54dd9bde719c2bb0/latest/USD"
+var TIME_DAY = 24 * time.Hour
+var DATA_HISTORY map[time.Time]float64
+
 type Data struct {
-	Curr string             `json:"date"`
-	Val  map[string]float64 `json:"rub"`
+	Date time.Time
+	Val  map[string]float64 `json:"conversion_rates"`
 }
 
-func FoundUSDT() float64 {
-	resp, err := http.Get("https://latest.currency-api.pages.dev/v1/currencies/rub.json")
+func init() {
+	DATA_HISTORY = make(map[time.Time]float64)
+}
+
+func FoundUSDT() (Data, error) {
+	resp, err := http.Get(ADRESS_API)
 	if err != nil {
 		fmt.Println(err)
-		return 0
+		return Data{}, err
 	}
 
 	defer resp.Body.Close()
@@ -25,32 +33,49 @@ func FoundUSDT() float64 {
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err)
-		return 0
+		return Data{}, err
 	}
 
 	answer := &Data{}
+	answer.Date = time.Now()
 	err = json.Unmarshal(body, answer)
 	if err != nil {
 		fmt.Println(err)
-		return 0
+		return Data{}, err
 	}
 
-	return answer.Val["usdt"]
+	DATA_HISTORY[answer.Date] = answer.Val["RUB"]
+
+	return *answer, nil
 }
 
 func main() {
 
-	ticker := time.NewTicker(24 * time.Hour)
+	ticker := time.NewTicker(TIME_DAY)
 
-	rate_usdt := FoundUSDT()
+	defer ticker.Stop()
 
-	select {
-	case <-ticker.C:
-		rate_usdt = FoundUSDT()
+	var rate_usdt float64
+
+	if rate, err := FoundUSDT(); err != nil {
+		fmt.Println("Ошибка при получении начального курса RUB:", err)
+	} else {
+		rate_usdt = rate.Val["RUB"]
 	}
 
+	go func() {
+		for range ticker.C {
+			if rate, err := FoundUSDT(); err != nil {
+				fmt.Println("Ошибка при получении начального курса RUB:", err)
+			} else {
+				rate_usdt = rate.Val["RUB"]
+			}
+		}
+	}()
+
 	http.HandleFunc("/curr", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Currency of USTD: ", rate_usdt)
+		fmt.Fprintln(w, "Date: ", time.DateTime)
+		fmt.Fprintln(w, "Currency of RUB: ", rate_usdt)
 	})
 
 	http.ListenAndServe(":8082", nil)
